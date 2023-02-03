7ae03b13cc5a1992a9b45a092fb6e33fdcc32b5a
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-02-03 16:42:59 +0900

    프로젝트 초기세팅 & 글로벌 스타일 & 폰트 추가하기

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+
+  input, button {
+    font-family: inherit;
+    border: none; 
+  }
+
+  input {
+    outline: none;
+  }  
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
